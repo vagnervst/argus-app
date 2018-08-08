16c0e24011b3d5423a11f64640e271c87a0f584d
Vagner S <vagnervst17@gmail.com>
2018-08-08 00:44:53 -0300

    add webpack-hot-middleware

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+require('dotenv').config()
 const path = require('path');
 const express = require('express');
+const webpack = require('webpack');
+const middleware = require('webpack-dev-middleware');
 const argus = require('@argus-dashboard/server');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-require('dotenv').config()
-
 const githubExtension = require('@argus-dashboard/github-extension/routes');
+
+const webpackConfig = require('./webpack.config');
+const compiler = webpack(webpackConfig);
 const routes = require('./routes');
 
 let app = express();
 
 app.use(express.static('public'));
+app.use(middleware(compiler));
+app.use(require('webpack-hot-middleware')(compiler));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
